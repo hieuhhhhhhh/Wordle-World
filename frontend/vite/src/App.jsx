95c9ddfd5f283ef6d7ae71b9f730bf46916e16b3
hieuhhhhhhh <hieuhaihuoc31102@gmail.com>
2024-04-28 19:23:35 -0400

    Update App.jsx

--- frontend/vite/src/App.jsx
+++ frontend/vite/src/App.jsx
@@ -1,12 +1,29 @@
 // App.js
 
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "./screens/Home";
+import About from "./screens/Home";
 
 function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+      </div>
+    </Router>
   );
 }
 
