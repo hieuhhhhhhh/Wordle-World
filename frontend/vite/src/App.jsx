9aa0cb651b85f993fd9c6ea26dd994c8b4691e82
hieuhhhhhhh <hieuhaihuoc31102@gmail.com>
2024-04-28 20:43:01 -0400

    reset3

--- frontend/vite/src/App.jsx
+++ frontend/vite/src/App.jsx
@@ -1,30 +1,45 @@
 // App.js
 
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./screens/Home";
-import About from "./screens/Home";
+import About from "./screens/About";
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
+      <nav style={styles.navStyle}>
+        <Link to="/" style={styles.tabStyle}>
+          Home
+        </Link>
+        <Link to="/about" style={styles.tabStyle}>
+          About
+        </Link>
+      </nav>
 
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-      </div>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </Router>
   );
 }
+const styles = {
+  navStyle: {
+    display: "flex",
+    justifyContent: "flex-start",
+    backgroundColor: "#f0f0f0", // Background color added
+    padding: "10px", // Added padding for better spacing
+  },
+
+  tabStyle: {
+    padding: "10px",
+    marginRight: "10px",
+    textDecoration: "none",
+    color: "black",
+    border: "1px solid #ccc",
+    borderRadius: "5px",
+  },
+};
 
 export default App;
