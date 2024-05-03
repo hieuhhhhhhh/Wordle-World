1d2b04092088f05ef7628cfce9ade8e19cd3ea28
hieuhhhhhhh <hieuhaihuoc31102@gmail.com>
2024-05-03 14:09:49 -0400

    update last commit

--- backend/src/routes/helpers/setTokenTO.js
+++ backend/src/routes/helpers/setTokenTO.js
@@ -2,26 +2,28 @@ import * as DBroutines from "backend/DBroutines";
 import * as config from "backend/config";
 
 const setTokenTimeout = async (token) => {
-  try {
-    // 1: connect db
-    const db = await DBroutines.getDB();
+  setTimeout(async () => {
+    try {
+      // 1: connect db
+      const db = await DBroutines.getDB();
 
-    // 2: compare data from database (2 expiry times should match)
-    const tokenInDB = await DBroutines.getDoc(db, "tokens", { _id: token._id });
+      // 2: get token data from database
+      const tokenInDB = await DBroutines.getDoc(db, "tokens", {
+        _id: token._id,
+      });
 
-    // 3: if not match, return
-    if (tokenInDB.expiry !== token.expiry) {
-      return;
-    }
+      // 3: if not match, return
+      if (tokenInDB.expiry.getTime() !== token.expiry.getTime()) {
+        return;
+      }
 
-    // 4: if matches, remove token after expiry time
-    setTimeout(async () => {
+      // 4: if matches, remove token
       await DBroutines.removeDoc(db, "tokens", { _id: token._id });
       console.log(`session timeout, user: ${token.username}`);
-    }, config.tokenTimeout * 60 * 1000);
-  } catch (error) {
-    console.error("Error login:", error);
-  }
+    } catch (error) {
+      console.error("Error handle token:", error);
+    }
+  }, config.tokenTimeout * 60 * 1000);
 };
 
 export default setTokenTimeout;
