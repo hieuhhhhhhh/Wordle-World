1f9db03e62bc76fa0fc4b1f858e428fc00da5065
hieuhhhhhhh <hieuhaihuoc31102@gmail.com>
2024-05-03 13:59:54 -0400

    Update setTokenTO.js

--- backend/src/routes/helpers/setTokenTO.js
+++ backend/src/routes/helpers/setTokenTO.js
@@ -2,8 +2,19 @@ import * as DBroutines from "backend/DBroutines";
 import * as config from "backend/config";
 
 const setTokenTimeout = async (token) => {
-  const db = await DBroutines.getDB();
   try {
+    // 1: connect db
+    const db = await DBroutines.getDB();
+
+    // 2: compare data from database (2 expiry times should match)
+    const tokenInDB = await DBroutines.getDoc(db, "tokens", { _id: token._id });
+
+    // 3: if not match, return
+    if (tokenInDB.expiry !== token.expiry) {
+      return;
+    }
+
+    // 4: if matches, remove token after expiry time
     setTimeout(async () => {
       await DBroutines.removeDoc(db, "tokens", { _id: token._id });
       console.log(`session timeout, user: ${token.username}`);
